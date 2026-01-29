5f4e3ade611c544584e7f579c12316eb473c4b0f
pesegato <pesegato@outlook.com>
2026-01-29 17:27:07 +0100

    wip

---
+++ common/src/main/kotlin/com/pesegato/security/Config.kt
@@ -0,0 +1,23 @@
+package com.pesegato.security
+
+import java.util.Properties
+
+object Config {
+    @JvmStatic
+    var algorithm: String = "PKCS1"
+    @JvmStatic
+    var idMode: String = "KEYRING"
+    @JvmStatic
+    var tpmStatus: String = "Unknown"
+    @JvmStatic
+    var isDocker: Boolean = System.getenv("RUNNING_IN_DOCKER") == "true"
+    @JvmStatic
+    var configLoaded: Boolean = false
+
+    fun loadFromProperties(props: Properties) {
+        algorithm = props.getProperty("tpm.algorithm", "PKCS1")
+        idMode = props.getProperty("tpm.id_mode", "KEYRING")
+        tpmStatus = props.getProperty("tpm.hw_verified", "Unknown")
+        configLoaded = true
+    }
+}
