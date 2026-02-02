b05fe112b5a753ce5929dcbde963ce73c7430db0
pesegato <pesegato@outlook.com>
2026-02-02 14:49:26 +0100

    wip

--- gui/build.gradle.kts
+++ gui/build.gradle.kts
@@ -13,6 +13,8 @@ dependencies {
     implementation(compose.materialIconsExtended)
     implementation("com.github.mobile-dev-inc:dadb:1.2.10")
     implementation(compose.material3)
+    //forse non serve
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.8.1")
 }
 
 compose.desktop {
@@ -21,7 +23,13 @@ compose.desktop {
         nativeDistributions {
             targetFormats(org.jetbrains.compose.desktop.application.dsl.TargetFormat.Dmg, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Msi, org.jetbrains.compose.desktop.application.dsl.TargetFormat.Deb)
             packageName = "Triceratops"
-            packageVersion = project.version.toString()
+            //packageVersion = project.version.toString()
+// Forza una versione numerica valida per gli standard OS (Debian/MSI/Dmg)
+            packageVersion = "1.1.0"
+
+            linux { debPackageVersion = "1.1.0" }
+            windows { msiPackageVersion = "1.1.0" }
+            macOS { dmgPackageVersion = "1.1.0" }
         }
     }
 }
