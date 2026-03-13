6fedfd606b0db9efc5054f85c404628c434a07c1
pesegato <pesegato@outlook.com>
2026-03-13 17:55:35 +0100

    wip

--- common/src/main/java/com/pesegato/data/Certificate.java
+++ common/src/main/java/com/pesegato/data/Certificate.java
@@ -2,7 +2,11 @@ package com.pesegato.data;
 
 import com.pesegato.device.DeviceManager;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -17,6 +21,28 @@ public class Certificate {
         this.name = name;
         this.publicKey = publicKey;
         this.hwid = DeviceManager.hwId;
+    }
+
+    public static byte[] serialize(String name, String hwid, byte[] pubKey) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        DataOutputStream dos = new DataOutputStream(baos);
+
+        // Scrivi Name
+        byte[] nameBytes = name.getBytes(StandardCharsets.UTF_8);
+        dos.writeByte(nameBytes.length); // Lunghezza in 1 byte (max 256)
+        dos.write(nameBytes);
+
+        // Scrivi HWID
+        byte[] hwidBytes = hwid.getBytes(StandardCharsets.UTF_8);
+        dos.writeByte(hwidBytes.length);
+        dos.write(hwidBytes);
+
+        // Scrivi PubKey
+        dos.writeInt(pubKey.length); // Lunghezza in 4 byte
+        dos.write(pubKey);
+
+        dos.close();
+        return baos.toByteArray();
     }
 
     public String getName() {
